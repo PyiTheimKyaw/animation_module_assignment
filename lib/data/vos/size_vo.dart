a9eb437125760513f182034e8480f336938f1924
PyiTheimKyaw <92578990+PyiTheimKyaw@users.noreply.github.com>
2022-05-25 00:26:59 +0630

    Completed animation assignment

---
+++ lib/data/vos/size_vo.dart
@@ -0,0 +1,6 @@
+class SizeVO{
+  String? size;
+  bool? isSelected;
+
+  SizeVO({required this.size, this.isSelected=false});
+}
--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors,prefer_const_literals_to_create_immutables, sized_box_for_whitespace, prefer_final_fields
 
+import 'package:animation_module_assignment/data/vos/size_vo.dart';
 import 'package:animation_module_assignment/utils/constants.dart';
 import 'package:flutter/material.dart';
 
+const kAnimationDuration = Duration(milliseconds: 500);
 const kAnimationDurationForFavourite = const Duration(milliseconds: 1000);
 
 class DetailsPage extends StatefulWidget {
@@ -13,17 +15,44 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late AnimationController _animationController;
+  late AnimationController _animationControllerForFavIcon;
+  late AnimationController _animationControllerForSize;
   late Animation<Color?> colorAnimation;
+  late Animation<Color?> colorAnimationForSize;
   bool isAnimationCompleted = true;
+  bool isDescriptionShown = true;
+  List<SizeVO> size = [
+    SizeVO(size: "S"),
+    SizeVO(size: "M"),
+    SizeVO(size: "L"),
+    SizeVO(size: "XL"),
+    SizeVO(size: "XXL"),
+  ];
+  bool isSizeSelected = false;
 
   @override
   void initState() {
     _animationController = AnimationController(
-        vsync: this, duration: kAnimationDurationForFavourite);
+      vsync: this,
+      duration: kAnimationDurationForFavourite,
+      upperBound: 0.5,
+    );
+    _animationControllerForSize = AnimationController(
+      vsync: this,
+      duration: kAnimationDurationForFavourite,
+      upperBound: 0.5,
+    );
+    _animationControllerForFavIcon = AnimationController(
+      vsync: this,
+      duration: kAnimationDurationForFavourite,
+      upperBound: 0.5,
+    );
     colorAnimation = ColorTween(begin: Colors.white, end: Colors.red)
-        .animate(_animationController.view);
+        .animate(_animationControllerForFavIcon.view);
+    colorAnimationForSize = ColorTween(begin: Colors.white, end: Colors.purple)
+        .animate(_animationControllerForSize.view);
     // _animationController.addStatusListener((status) {
     //   isAnimationCompleted = (status == AnimationStatus.completed);
     //   // _animationController.repeat();
@@ -33,7 +62,9 @@ class _DetailsPageState extends State<DetailsPage>
 
   @override
   void dispose() {
+    _animationControllerForSize.dispose();
     _animationController.dispose();
+    _animationControllerForFavIcon.dispose();
     super.dispose();
   }
 
@@ -47,39 +78,266 @@ class _DetailsPageState extends State<DetailsPage>
           mainAxisSize: MainAxisSize.min,
           children: [
             ItemProfileSecrionView(
-              animationController: _animationController,
+              animationController: _animationControllerForFavIcon,
               colorAnimation: colorAnimation,
               onTapFav: () {
                 setState(() {
                   isAnimationCompleted = !isAnimationCompleted;
                   if (isAnimationCompleted) {
-                    _animationController.reset();
+                    _animationControllerForFavIcon.reverse();
                   } else {
-                    _animationController.repeat();
+                    _animationControllerForFavIcon.repeat();
                   }
-                  // _animationController..reverse(from: 0.8);
                 });
               },
             ),
-            ItemDetailsSectionView(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: MARGIN_MEDIUM_2),
+              child: ItemDetailsSectionView(
+                isDescriptionShown: isDescriptionShown,
+                controller: _animationController,
+                controllerForSize: _animationControllerForSize,
+                onTapShowDescription: () {
+                  setState(() {
+                    isDescriptionShown = !isDescriptionShown;
+                    if (_animationController.isDismissed) {
+                      _animationController.forward();
+                    } else {
+                      _animationController.reverse();
+                    }
+                  });
+                },
+                isSizeSelected: isSizeSelected,
+                onTapSize: (index) {
+                  setState(() {
+                    size[index].isSelected = !(size[index].isSelected ?? false);
+                    // _animationControllerForSize.repeat();
+
+                    _animationControllerForSize.forward();
+                  });
+                },
+                size: size,
+                animationColor: colorAnimationForSize,
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
 class ItemDetailsSectionView extends StatelessWidget {
-  const ItemDetailsSectionView({Key? key}) : super(key: key);
+  ItemDetailsSectionView(
+      {required this.isDescriptionShown,
+      required this.onTapShowDescription,
+      required this.controller,
+      required this.size,
+      required this.isSizeSelected,
+      required this.onTapSize,
+      required this.animationColor,
+      required this.controllerForSize});
+
+  final bool isDescriptionShown;
+  final Function onTapShowDescription;
+  final AnimationController controller;
+  final AnimationController controllerForSize;
+
+  final List<SizeVO>? size;
+  final Function(int) onTapSize;
+  final bool isSizeSelected;
+
+  final Animation<Color?> animationColor;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text("Adidas Shoes",style: (TextStyle(fontSize: 22,fontWeight: FontWeight.bold)),),
-        SizedBox(height: MARGIN_MEDIUM_2,),
-        Text("\$ 19,39",style: TextStyle(color: Colors.purple,fontSize: MARGIN_MEDIUM_2),),
+        Text(
+          "Adidas Shoes",
+          style: (TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+        ),
+        SizedBox(
+          height: MARGIN_MEDIUM_2,
+        ),
+        Text(
+          "\$ 19,39",
+          style: TextStyle(color: Colors.purple, fontSize: MARGIN_MEDIUM_2),
+        ),
+        SizedBox(
+          height: kMARGIN18,
+        ),
+        DescriptionAndExpandIconView(
+            controller: controller, onTapShowDescription: onTapShowDescription),
+        SizedBox(
+          height: kMARGIN18,
+        ),
+        ItemDescriptionView(isDescriptionShown: isDescriptionShown),
+        SizedBox(
+          height: kMARGIN18,
+        ),
+        SizeLabelView(),
+        Container(
+          height: 120,
+          width: double.infinity,
+          child: ListView.builder(
+              padding: EdgeInsets.only(left: MARGIN_MEDIUM - 7),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 5,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return AnimatedBuilder(
+                  animation: controllerForSize.view,
+                  builder: (BuildContext context, Widget? child) {
+                    return SizeItemView(
+                      size: size?[index],
+                      index: index,
+                      isSizeSelected: size?[index].isSelected ?? false,
+                      onTapSize: onTapSize,
+                      color: animationColor.value,
+                      animationColor: animationColor,
+                    );
+                  },
+                );
+              }),
+        )
+      ],
+    );
+  }
+}
 
-        
+class SizeItemView extends StatelessWidget {
+  SizeItemView(
+      {required this.size,
+      required this.index,
+      required this.onTapSize,
+      this.isSizeSelected = false,
+      required this.color,
+      required this.animationColor});
+
+  final SizeVO? size;
+  final int index;
+  final Function(int) onTapSize;
+  final bool isSizeSelected;
+  final Color? color;
+  final Animation<Color?> animationColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: () {
+            onTapSize(index);
+          },
+          child: AnimatedContainer(
+            duration: kAnimationDuration,
+            child: Container(
+              // margin: const EdgeInsets.all(8.0),
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0.0, 1.0), //(x,y)
+                    blurRadius: 6.0,
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(20),
+                color: (isSizeSelected) ? color : Colors.white,
+              ),
+              child: Center(child: Text(size?.size ?? "")),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: kMARGIN18,
+        )
+      ],
+    );
+  }
+}
+
+class SizeLabelView extends StatelessWidget {
+  const SizeLabelView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "Size your size",
+          style: TextStyle(fontSize: kMARGIN18),
+        ),
+        Spacer(),
+        Text(
+          "Size Guide",
+          style: TextStyle(color: Colors.purple),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemDescriptionView extends StatelessWidget {
+  const ItemDescriptionView({
+    Key? key,
+    required this.isDescriptionShown,
+  }) : super(key: key);
+
+  final bool isDescriptionShown;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSize(
+      duration: kAnimationDuration,
+      child: Container(
+        width: double.infinity,
+        height: (isDescriptionShown) ? null : 0.0,
+        child: Text(
+          "Through sports, we have the power to change lives. Sports keep us fit. They keep us mindful. They bring us together. Athletes inspire us. They help us to get up and get moving. And sporting goods featuring the latest technologies help us beat our personal best.",
+          style: TextStyle(color: Colors.black54),
+        ),
+      ),
+    );
+  }
+}
+
+class DescriptionAndExpandIconView extends StatelessWidget {
+  const DescriptionAndExpandIconView({
+    Key? key,
+    required this.controller,
+    required this.onTapShowDescription,
+  }) : super(key: key);
+
+  final AnimationController controller;
+  final Function onTapShowDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "Description",
+          style: TextStyle(fontSize: kMARGIN18),
+        ),
+        Spacer(),
+        AnimatedBuilder(
+          animation: controller.view,
+          builder: (context, _) => RotationTransition(
+            turns: Tween(begin: 0.0, end: 1.0).animate(controller),
+            child: IconButton(
+              icon: Icon(Icons.expand_more, size: 32),
+              onPressed: () {
+                onTapShowDescription();
+              },
+            ),
+          ),
+        ),
       ],
     );
   }
@@ -126,8 +384,9 @@ class ItemProfileSecrionView extends StatelessWidget {
                   AnimatedBuilder(
                     animation: _animationController.view,
                     builder: (BuildContext context, Widget? child) {
-                      return Transform.rotate(
-                        angle: _animationController.value * 6.3,
+                      return RotationTransition(
+                        turns: Tween(begin: 0.0, end: 2.0)
+                            .animate(_animationController),
                         child: FavouriteIconButtonVIew(
                           onTapFav: () {
                             onTapFav();
